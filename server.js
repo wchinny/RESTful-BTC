67aa4e034ce80476621e6ecd960733743e8f1557
chin <chin@chins-MacBook-Pro.local>
2018-04-17 14:32:26 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+var app = require('./app');
+var port = process.env.PORT || 3000;
+
+var Coin = require('./coin/Coin');
+
+const request = require("request");
+const url = "https://api.coinmarketcap.com/v1/ticker/bitcoin/";
+
+function refresh()
+{ 
+
+    request.get(url, (error, response, body) => {
+		let json = JSON.parse(body);
+		// console.log(json);
+		console.log(json[0].price_usd);
+		Coin.create({
+			name: json[0].name,
+			abbreviation: json[0].symbol,
+			price_usd: json[0].price_usd
+		});
+	});
+
+    setTimeout(refresh, 600000);
+}
+
+refresh();
+
+var server = app.listen(port, function() {
+  console.log('Express server listening on port ' + port);
+});
